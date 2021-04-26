03628a68ce64bc9c4945d5d4be5edb1450730867
bharathi-i2i <bharathidasan@ideas2it.com>
2021-04-26 09:57:52 +0530

    spring boot with flyway db migration done.

---
+++ dog/dog/src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,4 @@
+INSERT INTO dog(name, age)VALUES( 'tommy',10);
+INSERT INTO dog(name, age)VALUES( 'sam',2);
+INSERT INTO dog(name, age)VALUES( 'jimmy',5);
+INSERT INTO dog(name, age)VALUES( 'blacky',9);
